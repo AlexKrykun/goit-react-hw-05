30a43e8c122b647a9f9dfaa72f7eff2db5382dcc
Alex <140958369+AlexKrykun@users.noreply.github.com>
2024-03-17 13:45:06 +0200

    Complete_1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,35 @@
+
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navigation from '../Navigation/Navigation.jsx';
+import NotFound from '../../pages/NotFound/NotFound.jsx';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage.jsx'));
+const MovieCast = lazy(() => import('../MovieCast/MovieCast.jsx'));
+const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews.jsx'));
+
+import css from './App.module.css';
+
+export default function App() {
+  return (
+    <>
+      <Toaster position='top-right' />
+      <Navigation />
+
+      <Suspense fallback={<div>LOADING PAGE...</div>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<MoviesPage />} />
+          <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
+            <Route path='cast' element={<MovieCast />} />
+            <Route path='reviews' element={<MovieReviews />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
